b6bc644213c980184ca58467af50984613d9ecb7
Hypnosss <398906857@qq.com>
2019-08-10 21:51:25 +0800

    修复了光源在墙中会发光的bug

--- main.js
+++ main.js
@@ -71,11 +71,9 @@ function light(x, y) {
   ctx2.fill();
   
   
+  // console.log(x, y)
   // draw shadow
-  let idx = -1;
   for(let block of blocks) {
-
-    idx++;
     // ctx2.beginPath();
     // ctx2.moveTo(0, 150);
     // ctx2.lineTo(0, 175);
@@ -91,57 +89,61 @@ function light(x, y) {
       bottomRight: {x: (block[0]+1) * 50, y: (block[1]+1) * 50},
     };
     // console.log(corners)
-  
-    let points = [];
-    for(let corner in corners) {
-      let dis = calDis(corners[corner].x, corners[corner].y, x, y);
-      // console.log(dis)
-      let xRatio = -(x - corners[corner].x) / dis;
-      let yRatio = -(y - corners[corner].y) / dis;
-      if(dis < lightRadius) {
-        let difValue = lightRadius - dis;
-        points.push({
-          dis: dis,
-          inside: {
-            x: corners[corner].x, 
-            y: corners[corner].y
-          },
-          outside: {
-            x: corners[corner].x + difValue * xRatio, 
-            y: corners[corner].y + difValue * yRatio
-          }
-        });
-      }
-    }
-
-    if(points.length == 4) {
-      points.sort((a, b) => a.dis - b.dis);
-      // console.log(points);
-      // console.log(points[2].outside)
-
-
+    if(x > block[0] * 50 && x < (block[0] + 1) * 50 && y > block[1] * 50 && y < (block[1] + 1) * 50) {
+      // console.log(1)
       ctx2.globalCompositeOperation = "destination-out";
-      // ctx2.fillStyle = "white";
-      // ctx2.fillText(0, points[0].inside.x, points[0].inside.y)
-      // ctx2.fillText(1, points[1].inside.x, points[1].inside.y)
-      // ctx2.fillText(2, points[2].inside.x, points[2].inside.y)
-
-      ctx2.fillStyle = "red";
-      ctx2.beginPath();
-      ctx2.moveTo(points[1].inside.x, points[1].inside.y);
-      ctx2.lineTo(points[0].inside.x, points[0].inside.y);
-      ctx2.lineTo(points[2].inside.x, points[2].inside.y);
-
-      ctx2.lineTo(points[2].outside.x, points[2].outside.y);
-      ctx2.lineTo(points[3].outside.x, points[3].outside.y);
-      ctx2.lineTo(points[1].outside.x, points[1].outside.y);
-
-      ctx2.lineTo(points[1].inside.x, points[1].inside.y);
-      ctx2.fill();
+      ctx2.fillRect(0, 0, 800, 800);
+      break;
+    } else {
+      let points = [];
+      for(let corner in corners) {
+        let dis = calDis(corners[corner].x, corners[corner].y, x, y);
+        // console.log(dis)
+        let xRatio = -(x - corners[corner].x) / dis;
+        let yRatio = -(y - corners[corner].y) / dis;
+        if(dis < lightRadius) {
+          let difValue = lightRadius - dis;
+          points.push({
+            dis: dis,
+            inside: {
+              x: corners[corner].x, 
+              y: corners[corner].y
+            },
+            outside: {
+              x: corners[corner].x + difValue * xRatio, 
+              y: corners[corner].y + difValue * yRatio
+            }
+          });
+        }
+      }
+  
+      if(points.length == 4) {
+        points.sort((a, b) => a.dis - b.dis);
+        // console.log(points);
+        // console.log(points[2].outside)
+  
+  
+        ctx2.globalCompositeOperation = "destination-out";
+        // ctx2.fillStyle = "white";
+        // ctx2.fillText(0, points[0].inside.x, points[0].inside.y)
+        // ctx2.fillText(1, points[1].inside.x, points[1].inside.y)
+        // ctx2.fillText(2, points[2].inside.x, points[2].inside.y)
+  
+        ctx2.fillStyle = "red";
+        ctx2.beginPath();
+        ctx2.moveTo(points[1].inside.x, points[1].inside.y);
+        ctx2.lineTo(points[0].inside.x, points[0].inside.y);
+        ctx2.lineTo(points[2].inside.x, points[2].inside.y);
+  
+        ctx2.lineTo(points[2].outside.x, points[2].outside.y);
+        ctx2.lineTo(points[3].outside.x, points[3].outside.y);
+        ctx2.lineTo(points[1].outside.x, points[1].outside.y);
+  
+        ctx2.lineTo(points[1].inside.x, points[1].inside.y);
+        ctx2.fill();
+      } 
     }
-    
   } 
-   
 }
 
 
